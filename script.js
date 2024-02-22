4f8bcc921e53a6a87b7455b60767f91bbee84ee2
pemulaco <154732483+pemulaco@users.noreply.github.com>
2024-02-23 00:20:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var yellow = document.getElementById('yellow');
+
+yellow.addEventListener('click', function() {
+  yellow.style.backgroundColor = 'yellow';
+});
+
+var grey= document.getElementById('grey');
+
+grey.addEventListener('click', function() {
+  yellow.style.backgroundColor = 'grey';
+});
